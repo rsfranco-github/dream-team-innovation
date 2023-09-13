b1c2ba3b4c407c55b44fec13c3376457bc73b330
rsfranco <raul.franco.meza@gmail.com>
2023-09-12 20:44:08 -0500

    Se agregar configuracion para consumir por parametros q

--- java/digitalnao/src/main/java/com/example/digitalnao/controller/GoogleScholarController.java
+++ java/digitalnao/src/main/java/com/example/digitalnao/controller/GoogleScholarController.java
@@ -2,6 +2,7 @@ package com.example.digitalnao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.digitalnao.model.ResponseGoogleScholar;
@@ -13,9 +14,9 @@ public class GoogleScholarController {
 	@Autowired
 	private GoogleScholarService service;
 	
-	@GetMapping("/organic_results")
-	public ResponseGoogleScholar getGoogleScholar(){
-		return this.service.getGoogleScholar();
+	@GetMapping("/google_scholar/{q}")
+	public ResponseGoogleScholar getGoogleScholar(@PathVariable String q){
+		return this.service.getGoogleScholar(q);
 	}
 
 }
--- java/digitalnao/src/main/java/com/example/digitalnao/service/GoogleScholarService.java
+++ java/digitalnao/src/main/java/com/example/digitalnao/service/GoogleScholarService.java
@@ -1,5 +1,8 @@
 package com.example.digitalnao.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,10 +15,21 @@ public class GoogleScholarService {
 	@Autowired
 	private RestTemplate clientRest;
 	
-	private static final String URL_SERPAPI="https://serpapi.com/search.json?engine=google_scholar&q=biology&api_key=ddb54e522ef6f37417062521e237b07e6388e26152be7d872c490cdec596dea8";
+	private static final String API_KEY="ddb54e522ef6f37417062521e237b07e6388e26152be7d872c490cdec596dea8";
+	
+	private static final String ENGINE="google_scholar";
+	
+	private static final String URL_SERPAPI="https://serpapi.com/search.json?engine={engine}&q={q}&api_key={api_key}";
 
-	public ResponseGoogleScholar getGoogleScholar() {
-		return clientRest.getForObject(URL_SERPAPI, ResponseGoogleScholar.class);
+	public ResponseGoogleScholar getGoogleScholar(String q) {
+		
+		Map<String, String> pathVariables=new HashMap<>();
+		
+		pathVariables.put("q", q);
+		pathVariables.put("engine", ENGINE);
+		pathVariables.put("api_key", API_KEY);
+		
+		return clientRest.getForObject(URL_SERPAPI, ResponseGoogleScholar.class,pathVariables);
 	}
 	
 }
